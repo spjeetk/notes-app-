9d9aeb50689cf54efce8c0a5af5f43c6d509adcf
spjeetk <85982034+spjeetk@users.noreply.github.com>
2022-10-01 12:02:48 +0530

    Add files via upload

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+
+    return (
+        <div className="app-header">
+            <h1>Quick Notes</h1>
+        </div>
+    )
+}
+
+export default Header
